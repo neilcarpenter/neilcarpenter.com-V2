a00899ea91994bee294d3ffc8460cb434f84b830
Neil Carpenter <neil.carpenter@unit9.com>
2014-12-23 00:52:10 +0000

    remove GA completely for now

--- wordpress/wp-content/themes/neil-v2/parts/shared/html-footer.php
+++ wordpress/wp-content/themes/neil-v2/parts/shared/html-footer.php
@@ -33,10 +33,10 @@ require('neil-v2-config.php');
 </body>
 
 <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  // (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  // (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  // m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  // })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
   // ga('create', window.config.GA_code, 'auto');
   // ga('send', 'pageview');
