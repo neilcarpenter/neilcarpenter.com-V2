56397d88dfc5f2a507344b279c5812978cd4ba6c
Neil Carpenter <neil.carpenter@unit9.com>
2015-01-12 00:48:58 +0000

    build

--- wordpress/wp-content/themes/neil-v2/parts/shared/html-footer.php
+++ wordpress/wp-content/themes/neil-v2/parts/shared/html-footer.php
@@ -16,14 +16,14 @@ require('neil-v2-config.php');
 				base_url        : "<?= site_url(); ?>",
 				base_url_assets : "<? bloginfo('template_directory'); ?>",
 				dependencies : {
-					pixi : "<? bloginfo('template_directory');?>/js/vendor/pixi.js"
+					pixi : "<? bloginfo('template_directory');?>/js/vendor/pixi-e4a3e9a8.js"
 				}
 			};
 
 		</script>
 
-		<script src="<? bloginfo('template_directory');?>/js/vendor/v.js"></script>
-		<script src="<? bloginfo('template_directory');?>/js/main.js"></script>
+		<script src="<? bloginfo('template_directory');?>/js/vendor/v-82609bf7.js"></script>
+		<script src="<? bloginfo('template_directory');?>/js/main-be00af51.js"></script>
 
 		<noscript>
 			<p>Sorry, JavaScript be needed</p>
