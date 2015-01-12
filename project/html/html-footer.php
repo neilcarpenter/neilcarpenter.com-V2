447178116cceb962f1e3960cc9ca8a3060459e84
Neil Carpenter <neil.carpenter@unit9.com>
2015-01-12 00:36:39 +0000

    enable analytics on live site

--- project/html/html-footer.php
+++ project/html/html-footer.php
@@ -11,7 +11,7 @@ require('neil-v2-config.php');
 
 			window.config = {
 				ENV             : "",
-				GA_code         : "",
+				GA_code         : window.location.host === 'neilcarpenter.com' ? "UA-16253496-4" : false,
 				base_path       : "<?= $base_path ?>",
 				base_url        : "<?= site_url(); ?>",
 				base_url_assets : "<? bloginfo('template_directory'); ?>",
@@ -30,13 +30,15 @@ require('neil-v2-config.php');
 		</noscript>
 
 		<script>
-		  // (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		  // (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		  // m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		  // })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+			if (window.config.GA_code) {
+				(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+				})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-		  // ga('create', window.config.GA_code, 'auto');
-		  // ga('send', 'pageview');
+				ga('create', window.config.GA_code, 'auto');
+				ga('send', 'pageview');
+			}
 		</script>
 
 	</div>
