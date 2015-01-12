772ac81fbb9890e9ffe2a7f58bc2deb1b1895692
Neil Carpenter <neil.carpenter@unit9.com>
2015-01-12 17:10:28 +0000

    better header anim-in

--- wordpress/neil-v2-config.php
+++ wordpress/neil-v2-config.php
@@ -9,6 +9,7 @@
 switch ($_SERVER['SERVER_NAME']) {
 	case 'localhost':
 	case '192.168.168.192':
+	case '192.168.168.209':
 	case 'local.v2.neilcarpenter.com':
 		$db_name   = 'neil_v2_local';
 		$db_user   = 'root';
