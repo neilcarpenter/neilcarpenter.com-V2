66e5ebe6f0fb4525db449e1506edacbbd9a8b199
Neil Carpenter <neil.carpenter@unit9.com>
2015-01-14 13:42:37 +0000

    add bw version of bg image

--- wordpress/neil-v2-config.php
+++ wordpress/neil-v2-config.php
@@ -10,6 +10,7 @@ switch ($_SERVER['SERVER_NAME']) {
 	case 'localhost':
 	case '192.168.168.192':
 	case '192.168.168.209':
+	case '192.168.168.212':
 	case 'local.v2.neilcarpenter.com':
 		$db_name   = 'neil_v2_local';
 		$db_user   = 'root';
