05c437e388a80d48a3b2fe60ba3790df848e80c4
Neil Carpenter <neil.carpenter@unit9.com>
2015-01-15 23:53:39 +0000

    add new preloader shapes to WP generated content

--- wordpress/wp-content/themes/neil-v2/pages/work.php
+++ wordpress/wp-content/themes/neil-v2/pages/work.php
@@ -27,7 +27,7 @@
 						<a href="<?= get_site_url(); ?>/work/<?= $project->post_name ?>/">
 							<span class="work-preview-thumb bg-image-wrapper" data-lazyimage="<?= $project->thumbnail ?>">
 								<span class="bg-image"></span>
-								<div class="preloader preloader-image show">
+								<div class="preloader preloader-image show" data-preloader-shape="<?= $project->preloader_shape ?>">
 									<span class="preloader-inner"><span class="mask" data-preloader-mask="inner"></span></span>
 									<span class="preloader-outer"><span class="mask" data-preloader-mask="outer"></span></span>
 								</div>
