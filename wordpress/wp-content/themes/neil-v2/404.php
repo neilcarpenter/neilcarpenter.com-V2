8e8dfef496b267a854ae844524410c7360576685
Neil Carpenter <neil.carpenter@unit9.com>
2015-01-17 18:07:43 +0000

    tidying, remove extra vendors

--- wordpress/wp-content/themes/neil-v2/404.php
+++ wordpress/wp-content/themes/neil-v2/404.php
@@ -13,7 +13,7 @@
 
 			<div class="content">
 				<p class="intro" data-page-transition>Sorry, can't find that page!</p>
-				<p data-page-transition>There's a good chance that whatever you are looking for got lost when I migrated to a new site. The old one is now available at <a href="http://v1.neilcarpenter.com/">http://v1.neilcarpenter.com/</a>, so you can try prefixing the current URL with "v1", and you may find what you were looking for...</p>
+				<p data-page-transition>There's a good chance that whatever you are looking for got lost when I migrated to a new site. The old one is now available at <a href="http://v1.neilcarpenter.com/">v1.neilcarpenter.com</a>, so you can try prefixing the current URL with "v1", and you may find what you were looking for...</p>
 			</div>
 			
 		</div>
