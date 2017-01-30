3d910e88c0601a489b8bf533af098533e85fd97d
Neil Carpenter <neilanthonycarpenter@gmail.com>
2017-01-30 22:30:46 +0000

    Update about / contact

--- wordpress/wp-content/themes/neil-v2/pages/about.php
+++ wordpress/wp-content/themes/neil-v2/pages/about.php
@@ -14,8 +14,8 @@
 			<div class="content">
 				<p class="intro" data-page-transition>My name is Neil and I like to build websites.</p>
 				<p class="intro-sub" data-page-transition>I stay on top of the latest standards-based web technologies to help create interesting, interactive and nice-looking interfaces and experiences.</p>
-				<p data-page-transition>I live in London with my <a href="http://twitter.com/annieannier" target="_blank" title="Annie reluctantly on Twitter">nice ginger girlfriend</a> and am currently technical lead at <a href="http://www.unit9.com/" title="UNIT9" target="_blank">UNIT9</a>, and also occasionally code as part of <a href="http://fluuu.id" target="_blank">FLUUUID</a>. Other than staring at a screen I occasionally do other stuff, I won't bore you with the details.</p>
-				<p data-page-transition>This site is built on a WordPress port of <a href="https://github.com/unit9/coffee-bone" target="_blank">coffee-bone</a>, with all code available on <a href="https://github.com/neilcarpenter/neilcarpenter.com-V2" target="_blank">GitHub</a>. You can find the previous version of this site <a href="http://v1.neilcarpenter.com/">here</a>, which has more content including some blog posts and labs experiments.</p>
+				<p data-page-transition>I live in London with my <a href="http://twitter.com/annieannier" target="_blank" title="Annie reluctantly on Twitter">nice ginger fiancée</a> and am currently technical director at <a href="http://www.codeandtheory.com/" target="_blank">Code and Theory</a>, previously technical lead at <a href="http://www.unit9.com/" title="UNIT9" target="_blank">UNIT9</a>. I also code as part of <a href="http://fluuu.id" target="_blank">FLUUUID</a>.</p>
+				<p data-page-transition>If you'd like to get in touch, please check the <a href="/contact">contact page</a>.</p>
 			</div>
 			
 		</div>
--- wordpress/wp-content/themes/neil-v2/pages/contact.php
+++ wordpress/wp-content/themes/neil-v2/pages/contact.php
@@ -12,14 +12,12 @@
 		<div class="page page-contact" data-template="page-contact">
 
 			<div class="content">
-				<p class="intro" data-page-transition>What up.</p>
+				<p class="intro" data-page-transition>Hello.</p>
 				<p class="intro-sub" data-page-transition>If you feel like getting in touch, please email me on <a href="mailto:hi@neilcarpenter.com" title="Email me">hi@neilcarpenter.com</a>, or try one of the social accounts below.</p>
 				<ul class="social-account-list">
 					<li data-page-transition><a href="http://twitter.com/neilcarpenter" target="_blank">Twitter</a></li>
-					<li data-page-transition><a href="http://github.com/neilcarpenter" target="_blank">Github</a></li>
+					<li data-page-transition><a href="http://github.com/neilcarpenter" target="_blank">GitHub</a></li>
 					<li data-page-transition><a href="http://uk.linkedin.com/pub/neil-carpenter/23/8b4/619" target="_blank">LinkedIn</a></li>
-					<li data-page-transition><a href="http://codepen.io/neilcarpenter" target="_blank">Codepen</a></li>
-					<li data-page-transition><a href="http://www.flickr.com/photos/neilanthonycarpenter/" target="_blank">Flickr</a></li>
 				</ul>
 			</divL>
 			
